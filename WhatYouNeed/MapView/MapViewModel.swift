59fccdd7911a2a7b3bf5f473af7617fb57d4cd4d
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-02 20:27:54 +0300

    dummyPins Person model CLLocation

--- WhatYouNeed/MapView/MapViewModel.swift
+++ WhatYouNeed/MapView/MapViewModel.swift
@@ -6,20 +6,21 @@
 //
 
 import Foundation
+import CoreLocation
 
 class MapViewModel {
     let dummyPins = [
         Person(name: "Alperen Yüksel",
                phone: "+90 545 469 00 05",
-               coordinate: CGPoint(x: 252.33332824707031, y: 279),
+               location: CLLocation(latitude: 37.57800853172095, longitude: 36.91264699537522),
                needs: ["Battaniye", "Şarj Aleti", "Erkek İç Çamaşırı", "Ayakkabı 42 Numara", "Pantolon", "Atlet"]),
         Person(name: "Ali Köse",
                phone: "+90 551 573 63 29",
-               coordinate: CGPoint(x: 29, y: 308.66665649414063),
+               location: CLLocation(latitude: 37.57741027154303, longitude: 36.906964213486056),
                needs: ["Diş Fırçası", "Diş Macunu", "Atlet", "Kazak", "Pantolon", "Şarj Aleti", "Kalem", "Defter"]),
         Person(name: "Şahnur Aslan ",
                phone: "+90 558 848 70 92",
-               coordinate: CGPoint(x: 83.333328247070313, y: 598),
+               location: CLLocation(latitude: 37.57157530005541, longitude: 36.9083467409224),
                needs: ["Kalem", "Defter", "İç Çamaşırı", "Atlet", "Mont", "Bot 38 Numara", "Ayakkabı 38 Numara"])
     ]
     let marasCoordinates = (37.5764759, 36.9112263)
