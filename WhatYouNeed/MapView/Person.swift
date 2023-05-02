3a57537f35daacd0a786ef0f6d8976a2a7dc787e
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-02 19:57:03 +0300

    Person model

---
+++ WhatYouNeed/MapView/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  WhatYouNeed
+//
+//  Created by Ömer Faruk Okumuş on 2.05.2023.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let phone: String
+    let coordinate: CGPoint
+    let needs: [String]
+}
