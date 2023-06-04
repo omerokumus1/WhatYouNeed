a783b25820c986f14c990507cf626143f0f84b07
Ömer Okumuş <omerokumus3@gmail.com>
2023-06-04 09:10:28 +0300

    Dummy NetworkService

--- WhatYouNeed/Controllers/MapView/MapViewModel.swift
+++ WhatYouNeed/Controllers/MapView/MapViewModel.swift
@@ -9,19 +9,10 @@ import Foundation
 import CoreLocation
 
 class MapViewModel {
-    var dummyPins = [
-        Person(name: "Alperen Yüksel",
-               phone: "+90 545 469 00 05",
-               location: CLLocation(latitude: 37.57800853172095, longitude: 36.91264699537522),
-               needs: ["Battaniye", "Şarj Aleti", "Erkek İç Çamaşırı", "Ayakkabı 42 Numara", "Pantolon", "Atlet"].joined(separator: ", ")),
-        Person(name: "Ali Köse",
-               phone: "+90 551 573 63 29",
-               location: CLLocation(latitude: 37.57741027154303, longitude: 36.906964213486056),
-               needs: ["Diş Fırçası", "Diş Macunu", "Atlet", "Kazak", "Pantolon", "Şarj Aleti", "Kalem", "Defter"].joined(separator: ", ")),
-        Person(name: "Şahnur Aslan ",
-               phone: "+90 558 848 70 92",
-               location: CLLocation(latitude: 37.57157530005541, longitude: 36.9083467409224),
-               needs: ["Kalem", "Defter", "İç Çamaşırı", "Atlet", "Mont", "Bot 38 Numara", "Ayakkabı 38 Numara"].joined(separator: ", "))
-    ]
+    var people = ObservableObject<[Person]>()
     let marasCoordinates = (37.5764759, 36.9112263)
+    
+    func fetchPeople() {
+        people.set(value: NetworkService.shared.fetchPins())
+    }
 }
--- WhatYouNeed/Network/NetworkService.swift
+++ WhatYouNeed/Network/NetworkService.swift
@@ -12,4 +12,32 @@ class NetworkService {
     
     private init() {}
     
+    
+    
+//    func fetchCurrentUser() {
+//        // CurrentUser.shared.set(value: )
+//    }
+//    
+    func fetchPins() -> [Person]{
+        return [
+            Person(id: "1", name: "Alperen Yüksel",
+                   phone: "+90 545 469 00 05",
+                   location: Location(lat: 37.57800853172095, long: 36.91264699537522), address: "",
+                   needs: ["Battaniye", "Şarj Aleti", "Erkek İç Çamaşırı", "Ayakkabı 42 Numara", "Pantolon", "Atlet"].joined(separator: ", ")),
+            Person(id:"2", name: "Ali Köse",
+                   phone: "+90 551 573 63 29",
+                   location: Location(lat: 37.57741027154303, long: 36.906964213486056), address: "",
+                   needs: ["Diş Fırçası", "Diş Macunu", "Atlet", "Kazak", "Pantolon", "Şarj Aleti", "Kalem", "Defter"].joined(separator: ", ")),
+            Person(id: "3", name: "Şahnur Aslan ",
+                   phone: "+90 558 848 70 92",
+                   location: Location(lat: 37.57157530005541, long: 36.9083467409224), address: "",
+                   needs: ["Kalem", "Defter", "İç Çamaşırı", "Atlet", "Mont", "Bot 38 Numara", "Ayakkabı 38 Numara"].joined(separator: ", "))
+        ]
+    }
+    
+    
+    func save(currentUser: Person) {
+        
+    }
+    
 }
